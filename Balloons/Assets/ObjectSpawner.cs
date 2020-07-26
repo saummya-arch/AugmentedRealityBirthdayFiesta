fbbccb8bd39831b3c7e19845573fd6e0d35c711a
saummya-arch <61094556+saummya-arch@users.noreply.github.com>
2020-07-26 13:05:53 +0530

    Add files via upload

---
+++ Balloons/Assets/ObjectSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+    private Placement placementIndicator;
+
+    void Start ()
+    {
+        placementIndicator = FindObjectOfType<Placement>();
+    }
+
+    void Update()
+    {
+        if( Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            GameObject obj = Instantiate(objectToSpawn, placementIndicator.transform.position, placementIndicator.transform.rotation);
+        }
+    }
+}
---
+++ Balloons/Assets/Placement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class Placement : MonoBehaviour
+{
+    private ARRaycastManager rayManager;
+    private GameObject visual;
+
+    void Start()
+        {
+        //get components
+        rayManager = FindObjectOfType<ARRaycastManager>();
+        visual = transform.GetChild(0).gameObject;
+
+        // hide the placement visual
+        visual.SetActive(false);
+
+    }
+
+    void Update()
+    {
+        // shoot a raycast from the center of the screen
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        rayManager.Raycast(new Vector2(Screen.width / 2, Screen.height /2), hits, TrackableType.Planes);
+
+        //if we hit an AR plane , update the position
+        if(hits.Count > 0){
+            transform.position = hits[0].pose.position;
+            transform.rotation = hits[0].pose.rotation;
+
+            if(!visual.activeInHierarchy){
+                visual.SetActive(true);
+            }
+        }
+
+    }
+
+      
+        
+    }
+
+
